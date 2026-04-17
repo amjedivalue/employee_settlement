39427c10cbc31e09f882eec40f754835db7494ee
amjedivalue <Amjed.Altamimi@iValueConsult.com>
2026-04-17 15:47:25 +0300

    Add Saudi end-of-service gratuity calculation in Full and Final
    - calculated based on total salary
    - applied Saudi law rules (0-5 years, 5+ years)
    - handle resignation cases
    - only for Saudi companies and permanent employees

--- new_ivalue_fnf/ivalue_fnf_new/doctype/full_and_final_component/full_and_final_component.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Amjad and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class FullandFinalComponent(Document):
-	pass
--- new_ivalue_fnf/ivalue_fnf_new/doctype/full_and_final_setting/full_and_final_setting.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Amjad and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Full and Final Setting", {
-// 	refresh(frm) {
-
-// 	},
-// });

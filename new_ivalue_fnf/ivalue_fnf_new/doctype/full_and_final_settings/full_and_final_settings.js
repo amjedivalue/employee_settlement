9ca7aa7e50f51b6b92b663965337a5ff103b357d
amjedivalue <Amjed.Altamimi@iValueConsult.com>
2026-04-18 16:02:28 +0300

    solve account problem in setting doctype

--- new_ivalue_fnf/ivalue_fnf_new/doctype/full_and_final_settings/full_and_final_settings.js
+++ new_ivalue_fnf/ivalue_fnf_new/doctype/full_and_final_settings/full_and_final_settings.js
@@ -2,7 +2,12 @@ frappe.ui.form.on("Full and Final Settings", {
     refresh: function (frm) {
         hide_add_row_button(frm);
         lock_component_key_column(frm);
+        set_account_query_for_company(frm);
         make_grid_clean_for_hr(frm);
+    },
+
+    company: function (frm) {
+        set_account_query_for_company(frm);
     }
 });
 
@@ -38,6 +43,30 @@ function lock_component_key_column(frm) {
     }
 
     grid.update_docfield_property("component_key", "read_only", 1);
+}
+
+
+function set_account_query_for_company(frm) {
+    if (!frm.fields_dict.components) {
+        return;
+    }
+
+    if (!frm.fields_dict.components.grid) {
+        return;
+    }
+
+    frm.fields_dict.components.grid.get_field("account").get_query = function (doc) {
+        if (!doc.company) {
+            return {};
+        }
+
+        return {
+            filters: {
+                company: doc.company,
+                is_group: 0
+            }
+        };
+    };
 }
 
 
